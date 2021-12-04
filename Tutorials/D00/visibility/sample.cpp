444b877e795fe0a4a4cfd76091c6bb7d0b1121a1
isaaclim <isaaclimjj@yahoo.com>
2021-12-04 23:05:58 +0800

    visibility created

---
+++ Tutorials/D00/visibility/sample.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Sample.class.hpp"
+
+int main()
+{
+
+	Sample instance;
+	instance.publicFoo = 42;
+	std::cout << "instance.publicFoo: " << instance.publicFoo << std::endl;
+	instance.publicBar();
+
+	//Privates doesn't work
+	// instance._privateFoo = 42;
+	// std::cout << "instance._privateFoo: " << instance._privateFoo << std::endl;
+	// instance._privateBar();
+}
