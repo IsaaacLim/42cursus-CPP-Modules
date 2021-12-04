52ee189f028a60b8245ba0711057664fbe386e75
isaaclim <isaaclimjj@yahoo.com>
2021-12-04 22:28:20 +0800

    added comments

--- Tutorials/D00/Sample2.class.cpp
+++ Tutorials/D00/Sample2.class.cpp
@@ -2,6 +2,7 @@
 #include "Sample2.class.hpp"
 
 //initialize attributes of class (initialization list)
+//Vocab: Initializing the a1 attribute to the p1 value
 Sample2::Sample2(char p1, int p2, float p3) : a1(p1), a2(p2), a3(p3) {
 
 	std::cout << "Constructor called" << std::endl;
