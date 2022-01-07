bd813a5d0e72369abfc47ae26cf669722f389fba
isaaclim <isaaclimjj@yahoo.com>
2022-01-07 11:58:21 +0800

    moved colors to main

--- Modules/04/ex00/main.cpp
+++ Modules/04/ex00/main.cpp
@@ -1,5 +1,13 @@
 #include "Poly.hpp"
 
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define BLUE "\033[0;34m"
+#define PURPLE "\033[0;35m"
+#define CYAN "\033[0;36m"
+#define RESET "\033[0m"
+
 int main()
 {
 	// Constructors --------------------
@@ -28,8 +36,8 @@ int main()
 	std::cout << RED "WRONGCAT\ngetType()\t: " << wrongcat->getType() << "\n";
 	std::cout << "makeSound()\t: ";
 	wrongcat->makeSound();
-	
-	std::cout << YELLOW "META\ngetType()\t: "<< meta->getType() << "\n";
+
+	std::cout << YELLOW "META\ngetType()\t: " << meta->getType() << "\n";
 	std::cout << "makeSound()\t: ";
 	meta->makeSound();
 
