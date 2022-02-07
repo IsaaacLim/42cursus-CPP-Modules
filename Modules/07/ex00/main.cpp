7939305e7bb24cef30e1d8210277fb5e91ddc543
Jinq Jia Lim <isaaclimjj@yahoo.com>
2022-02-07 14:27:27 +0800

    included intra testing during evaluation

--- Modules/07/ex00/main.cpp
+++ Modules/07/ex00/main.cpp
@@ -1,5 +1,27 @@
 #include "whatever.hpp"
 
+/*
+** Class given by 42 during the eval
+*/
+class Awesome
+{
+private:
+	int _n;
+public:
+	Awesome(void) : _n(0) {}
+	Awesome( int n ) : _n( n ) {}
+	Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+	bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+	bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+	bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+	bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+	bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+	bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+	int get_n() const { return _n; }
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+
 template <typename T>
 void ft_display(T const &x, T const &y, char var1, char var2)
 {
@@ -11,20 +33,31 @@ void ft_display(T const &x, T const &y, char var1, char var2)
 
 int main(void)
 {
-	int a = 2;
-	int b = 3;
-	ft_display(a, b, 'a', 'b');
-	::swap(a, b);
-	ft_display(a, b, 'a', 'b');
-	std::cout << "\n";
+	// My test
+	{
+		int a = 2;
+		int b = 3;
+		ft_display(a, b, 'a', 'b');
+		::swap(a, b);
+		ft_display(a, b, 'a', 'b');
+		std::cout << "\n";
 
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	ft_display(c, d, 'c', 'd');
-	std::cout << "\n";
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		ft_display(c, d, 'c', 'd');
+		std::cout << "\n";
 
-	float e = 42.24f;
-	float f = 42.24f;
-	ft_display(e, f, 'e', 'f');
+		float e = 42.24f;
+		float f = 42.24f;
+		ft_display(e, f, 'e', 'f');
+		std::cout << "\n";
+	}
+	// Intra test
+	{
+		Awesome a(2), b(4);
+
+		swap(a, b);
+		ft_display(a, b, 'a', 'b');
+	}
 }
