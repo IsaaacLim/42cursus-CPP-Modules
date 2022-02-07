9d9681ef3e7ece66024366521ae79f89991ca1ba
Jinq Jia Lim <isaaclimjj@yahoo.com>
2022-02-07 17:47:41 +0800

    added test for List

--- Modules/08/ex01/main.cpp
+++ Modules/08/ex01/main.cpp
@@ -1,4 +1,5 @@
 #include "Span.hpp"
+#include <list>
 #include <string.h> //strncmp
 
 void ft_printTitle(std::string title, char c)
@@ -85,16 +86,27 @@ int main(int argc, char *argv[])
 	ft_printArr(sp3);
 	ft_findSpan(sp3);
 
+	//***** ADDING w/ RANGE OF ITERATORS *************************************//
+	Span sp4(5);
+	std::list<int> tmp2;
+
+	ft_printTitle("Adding with Iterators (List)", '\n');
+	for (int i = 0; i < 5; i++)
+		tmp2.push_back(i);
+	sp4.addNumberRange(tmp2.begin(), tmp2.end());
+	ft_printArr(sp4);
+	ft_findSpan(sp4);
+
 	// ***** 10000 NUMBERS TEST ********************************************* //
-	Span sp4(10000);
-	std::vector<int> tmp2(10000, 0);
+	Span sp5(10000);
+	std::vector<int> tmp3(10000, 0);
 
 	ft_printTitle("10000 Numbers Test", '\n');
-	generate(tmp2.begin(), tmp2.end(), rand);
-	sp4.addNumberRange(tmp2.begin(), tmp2.end());
+	generate(tmp3.begin(), tmp3.end(), rand);
+	sp5.addNumberRange(tmp3.begin(), tmp3.end());
 	if (argc == 2 && !(strncmp(argv[1], "show", 5)))
-		ft_printArr(sp4);
+		ft_printArr(sp5);
 	else
 		std::cout << "\033[3;33mrun <./span show> to display container values\033[0m\n";
-	ft_findSpan(sp4);
+	ft_findSpan(sp5);
 }
